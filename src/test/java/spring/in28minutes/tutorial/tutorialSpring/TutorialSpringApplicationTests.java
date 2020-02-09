023de3d3904dbb4d5aa7413af97984df59187b92
Vito <maultosar@gmail.com>
2020-02-09 17:55:20 +0100

    Initial commit

---
+++ src/test/java/spring/in28minutes/tutorial/tutorialSpring/TutorialSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.in28minutes.tutorial.tutorialSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
